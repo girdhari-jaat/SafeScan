01757e53d814722989c6843b031507fb567debda
girdhari-jaat <girdharijaat@gmail.com>
2026-06-27 10:30:00 +0500

    Live data

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -117,6 +117,7 @@ dependencies {
     implementation("androidx.compose.material3:material3")
     implementation("androidx.compose.ui:ui-tooling-preview")
     implementation("androidx.activity:activity-compose:1.8.2")
+    implementation("androidx.compose.runtime:runtime-livedata:1.6.8")
 
     // Hilt
     implementation("com.google.dagger:hilt-android:2.52")
