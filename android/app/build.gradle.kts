d6cef11acb4100a4b929f8ae691bc63e702c7a05
girdhari-jaat <girdharijaat@gmail.com>
2026-06-27 08:50:05 +0500

    Commit

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -96,7 +96,7 @@ dependencies {
     implementation("androidx.camera:camera-view:$camerax_version")
 
     // ML Kit
-    implementation("com.google.mlkit:text-recognition-latin:16.0.0")
+    implementation("com.google.android.gms:play-services-mlkit-text-recognition:19.0.0")
     implementation("com.google.android.gms:play-services-mlkit-barcode-scanning:18.3.0")
 
     // Lifecycle/ViewModel/Coroutines
