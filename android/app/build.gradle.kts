047f927ef24f748216d18973ed004290b8306fe3
girdhari-jaat <girdharijaat@gmail.com>
2026-06-29 12:39:12 +0500

    Update build.gradle.kts

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -100,7 +100,7 @@ dependencies {
     // ML Kit
     implementation("com.google.android.gms:play-services-mlkit-text-recognition:19.0.0")
     implementation("com.google.android.gms:play-services-mlkit-barcode-scanning:18.3.0")
-    implementation("com.google.mlkit:document-scanner:16.0.0-beta4")
+    implementation("com.google.mlkit:document-scanner:16.0.0")
     implementation("com.google.mlkit:object-detection:17.0.2")
     implementation("com.google.mlkit:object-detection-custom:17.0.2")
 
