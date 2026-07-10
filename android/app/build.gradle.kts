e2a0bdd49b44ec642207ef3c3565f4074779ae0e
girdhari-jaat <girdharijaat@gmail.com>
2026-07-10 23:13:08 +0500

    Lint

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -6,6 +6,8 @@ plugins {
     id("org.jetbrains.kotlin.android")
     id("kotlin-kapt")
     id("com.google.dagger.hilt.android")
+    id("io.gitlab.arturbosch.detekt")
+    id("org.jllewellyn.ktlint")
 }
 
 android {
--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -3,4 +3,6 @@ plugins {
     id("com.android.application") version "8.3.2" apply false
     id("org.jetbrains.kotlin.android") version "1.9.24" apply false
     id("com.google.dagger.hilt.android") version "2.52" apply false
+    id("io.gitlab.arturbosch.detekt") version "1.23.6"
+    id("org.jllewellyn.ktlint") version "12.1.0"
 }
